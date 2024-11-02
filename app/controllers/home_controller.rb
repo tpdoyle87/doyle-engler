d8a2d6285c0c47fd7d7fc605c64176de3164d9f0
Thomas DoyleEngler <tpdoyle87@gmail.com>
2024-11-02 18:05:29 -0400

    updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @projects = [ "item1", "item2", "item3" ]
-    @posts = Post.last(3)
+    @posts = Post.order(created_at: :desc).limit(3)
     @contact = Contact.new
   end
 end
